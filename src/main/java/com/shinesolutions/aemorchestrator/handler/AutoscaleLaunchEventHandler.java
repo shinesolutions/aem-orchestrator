776d7b5f18f774aaa28879e3f9191f24b544b1a5
nletts <nick.letts@gmail.com>
2017-01-27 14:26:44 +1100

    Added error handling for missing or invalid AWS group autoscaling names

--- src/main/java/com/shinesolutions/aemorchestrator/handler/AutoscaleLaunchEventHandler.java
+++ src/main/java/com/shinesolutions/aemorchestrator/handler/AutoscaleLaunchEventHandler.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.shinesolutions.aemorchestrator.actions.ScaleAction;
@@ -11,13 +13,29 @@ import com.shinesolutions.aemorchestrator.model.EventMessage;
 
 @Component
 public class AutoscaleLaunchEventHandler implements EventHandler {
-    
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Resource
     private Map<String, ScaleAction> scaleUpAutoScaleGroupMappings;
 
     public boolean handleEvent(EventMessage message) {
-        ScaleAction action = scaleUpAutoScaleGroupMappings.get(message.getAutoScalingGroupName());
-        return action.execute(message.getEC2InstanceId());
+        boolean success = false;
+        try {
+            ScaleAction action = scaleUpAutoScaleGroupMappings.get(message.getAutoScalingGroupName());
+            if (action == null) {
+                logger.warn(
+                    "Unable to find 'scale up' action for auto scaling group name: " + message.getAutoScalingGroupName()
+                        + ". Please check the correct auto scaling group names were added to the application config");
+            } else {
+                success = action.execute(message.getEC2InstanceId());
+            }
+        } catch (Exception e) {
+            logger.error("Failed to execute 'scale up' action for auto scaling group name: " + 
+                message.getAutoScalingGroupName(), e);
+        }
+
+        return success;
     }
 
 }
--- src/main/java/com/shinesolutions/aemorchestrator/handler/AutoscaleTerminateEventHandler.java
+++ src/main/java/com/shinesolutions/aemorchestrator/handler/AutoscaleTerminateEventHandler.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.shinesolutions.aemorchestrator.actions.ScaleAction;
@@ -12,12 +14,28 @@ import com.shinesolutions.aemorchestrator.model.EventMessage;
 @Component
 public class AutoscaleTerminateEventHandler implements EventHandler {
     
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
     @Resource
     private Map<String, ScaleAction> scaleDownAutoScaleGroupMappings;
 
     public boolean handleEvent(EventMessage message) {
-        ScaleAction action = scaleDownAutoScaleGroupMappings.get(message.getAutoScalingGroupName());
-        return action.execute(message.getEC2InstanceId());
+        boolean success = false;
+        try {
+            ScaleAction action = scaleDownAutoScaleGroupMappings.get(message.getAutoScalingGroupName());
+            if (action == null) {
+                logger.warn(
+                    "Unable to find 'scale down' action for auto scaling group name: " + message.getAutoScalingGroupName()
+                        + ". Please check the correct auto scaling group names were added to the application config");
+            } else {
+                success = action.execute(message.getEC2InstanceId());
+            }
+        } catch (Exception e) {
+            logger.error("Failed to execute 'scale down' action for auto scaling group name: " + 
+                message.getAutoScalingGroupName(), e);
+        }
+
+        return success;
     }
 
 }
