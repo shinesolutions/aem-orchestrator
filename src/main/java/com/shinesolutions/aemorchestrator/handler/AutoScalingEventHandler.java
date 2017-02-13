ef5affb3ddf51bfac5d89ad665657993c36860d8
nletts <nick.letts@gmail.com>
2017-02-13 11:04:26 +1100

    Changed ScaleUp and ScaleDown event handlers to use the same base code

--- src/main/java/com/shinesolutions/aemorchestrator/handler/AutoScalingEventHandler.java
+++ src/main/java/com/shinesolutions/aemorchestrator/handler/AutoScalingEventHandler.java
@@ -2,49 +2,51 @@ package com.shinesolutions.aemorchestrator.handler;
 
 import java.util.Map;
 
-import javax.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import com.shinesolutions.aemorchestrator.actions.ScaleAction;
 import com.shinesolutions.aemorchestrator.model.EventMessage;
 import com.shinesolutions.aemorchestrator.service.AwsHelperService;
 
-@Component
-public class AutoscaleLaunchEventHandler implements EventHandler {
-
+public class AutoScalingEventHandler implements EventHandler {
+    
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Resource
-    private Map<String, ScaleAction> scaleUpAutoScaleGroupMappings;
+    private Map<String, ScaleAction> autoScaleActionGroupMappings;
     
-    @Resource
     private AwsHelperService awsHelperService;
 
+    public AutoScalingEventHandler(Map<String, ScaleAction> autoScaleActionGroupMappings,
+        AwsHelperService awsHelperService) {
+        super();
+        this.autoScaleActionGroupMappings = autoScaleActionGroupMappings;
+        this.awsHelperService = awsHelperService;
+    }
+
+    @Override
     public boolean handleEvent(EventMessage message) {
         boolean success = false;
         try {
-            ScaleAction action = scaleUpAutoScaleGroupMappings.get(message.getAutoScalingGroupName());
+            ScaleAction action = autoScaleActionGroupMappings.get(message.getAutoScalingGroupName());
             if (action == null) {
                 logger.warn(
-                    "Unable to find 'scale up' action for auto scaling group name: " + message.getAutoScalingGroupName()
+                    "Unable to find action for auto scaling group name: " + message.getAutoScalingGroupName()
                         + ". Please check the correct auto scaling group names were added to the application config");
             } else {
                 if(awsHelperService.isInstanceRunning(message.getEC2InstanceId())) {
                     success = action.execute(message.getEC2InstanceId());
                 } else {
-                    logger.warn("Instance " + message.getEC2InstanceId() + " does not appear to be running. Ignoring 'scale up' action");
+                    logger.warn("Instance " + message.getEC2InstanceId() + 
+                        " does not appear to be running. Ignoring message (no action taken)");
                     success = true;
                 }
             }
         } catch (Exception e) {
-            logger.error("Failed to execute 'scale up' action for auto scaling group name: " + 
+            logger.error("Failed to execute " + message.getEvent() + " action for auto scaling group name: " + 
                 message.getAutoScalingGroupName(), e);
         }
 
         return success;
     }
-
 }
