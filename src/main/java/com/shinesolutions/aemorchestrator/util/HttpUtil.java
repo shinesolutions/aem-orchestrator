0917745fb997d43ff3663029caab2b5253c7f357
nletts <nick.letts@gmail.com>
2017-04-03 09:34:26 +1000

    AEM health check not longer verifies SSL hostname

--- src/main/java/com/shinesolutions/aemorchestrator/util/HttpUtil.java
+++ src/main/java/com/shinesolutions/aemorchestrator/util/HttpUtil.java
@@ -1,13 +1,20 @@
 package com.shinesolutions.aemorchestrator.util;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.stereotype.Component;
 
 /**
@@ -23,9 +30,21 @@ public class HttpUtil {
      * @return HTTP status code (integer form)
      * @throws IOException (normally if can't connect)
      * @throws ClientProtocolException if there's an error in the HTTP protocol
+     * @throws KeyStoreException if there is an error with the key store
+     * @throws NoSuchAlgorithmException if a cryptographic algorithm is request, but not supported
+     * @throws KeyManagementException if any issues with key management
      */
-    public boolean isHttpGetResponseOk(String url) throws ClientProtocolException, IOException {
-        HttpClient client = HttpClientBuilder.create().build();
+    public boolean isHttpGetResponseOk(String url) throws ClientProtocolException, IOException, 
+        KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+        
+        SSLContext sslContext = new SSLContextBuilder()
+            .loadTrustMaterial(null, (certificate, authType) -> true).build();
+       
+          CloseableHttpClient client = HttpClients.custom()
+            .setSSLContext(sslContext)
+            .setSSLHostnameVerifier(new NoopHostnameVerifier())
+            .build();
+        
         HttpGet request = new HttpGet(url);
         
         HttpResponse response = client.execute(request);
