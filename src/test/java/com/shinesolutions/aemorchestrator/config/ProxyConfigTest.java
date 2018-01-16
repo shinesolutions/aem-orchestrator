2d687cafa19accdc74b1edc32ba68c1d9ef712c8
jimmyting44 <jimmyting93@hotmail.com>
2018-01-16 15:59:45 +1100

    Add unit tests for ProxyConfig.

---
+++ src/test/java/com/shinesolutions/aemorchestrator/config/ProxyConfigTest.java
@@ -0,0 +1,25 @@
+package com.shinesolutions.aemorchestrator.config;
+
+import com.shinesolutions.aemorchestrator.model.ProxyDetails;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.nullValue;
+
+public class ProxyConfigTest {
+
+    private ProxyConfig proxyConfig;
+
+    @Before
+    public void setup() {
+        proxyConfig = new ProxyConfig();
+    }
+
+    @Test
+    public void testProxyDetails_NoEnvironmentVariable() {
+        ProxyDetails details = proxyConfig.proxyDetails();
+
+        assertThat(details, nullValue());
+    }
+}
